66580023e6a32d3423be6df3820c55c4ae66e07a
Rémi Lebret <remi.lebret@ext.adeoservices.com>
2015-11-04 16:31:41 +0100

    KO

--- controllers/index.js
+++ controllers/index.js
@@ -7,11 +7,29 @@ module.exports = function (router) {
 
     var model = new IndexModel();
 
+    model.messages=['firstMessage','secondMessage']
+
+    model.messagesHelper=function(chunk, context, bodies, params) {
+	    if( Object.prototype.toString.call( this.messages ) === '[object Array]' ) {
+	        var messages = context.get("messages");
+
+	        messages.forEach(function(message) {
+	            chunk.render(bodies.block, context.push({
+	                "msgCode": message
+	            }));
+	        });
+	    }
+	}
+
     router.get('/', function (req, res) {
-        
         
         res.render('index', model);
         
+    });
+
+    router.get('/ko', function (req, res) {
+        
+        res.render('index_ko', model);
         
     });
 
