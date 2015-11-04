77c8b714236373937dbb5c82cad95962505e2f6f
Matt Edelman <medelman@paypal.com>
2015-11-04 10:51:06 -0800

    modify to use bundalo

--- controllers/index.js
+++ controllers/index.js
@@ -1,36 +1,41 @@
 'use strict';
 
 var IndexModel = require('../models/index');
+var bundalo = require('bundalo');
 
+//couple of configs for later
+var config = {
+    "contentPath": "locales/", //required
+    "fallback": "en-US",       //optional
+    "engine": "dust",          //required
+    "cache": false             //optional, default is true
+};
+
+var bundle = bundalo(config);
 
 module.exports = function (router) {
 
     var model = new IndexModel();
 
-    model.messages=['firstMessage','secondMessage']
+    model.messages=['firstMessage', 'secondMessage'];
 
-    model.messagesHelper=function(chunk, context, bodies, params) {
-	    if( Object.prototype.toString.call( this.messages ) === '[object Array]' ) {
-	        var messages = context.get("messages");
-
-	        messages.forEach(function(message) {
-	            chunk.render(bodies.block, context.push({
-	                "msgCode": message
-	            }));
-	        });
-	    }
-	}
 
     router.get('/', function (req, res) {
-        
+
         res.render('index', model);
-        
+
     });
 
     router.get('/ko', function (req, res) {
-        
-        res.render('index_ko', model);
-        
+        model.tmessages=[];
+        bundle.get({'bundle': 'index_ko','locality': 'en-US', 'model': {}}, function bundaloReturn(err, data) {
+            model.messages.forEach(function (message) {
+                model.tmessages.push(data[message]);
+            });
+            res.render('index_ko', model);
+        });
+
+
     });
 
 };
